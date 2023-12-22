224ec413ccd4faa48a100c1655495bdfb5d1d46f
cenfun <cenfun@gmail.com>
2023-12-22 15:57:22 +0800

    update node test

--- test/test-v8-node.js
+++ test/test-v8-node.js
@@ -29,55 +29,60 @@ const generate = async () => {
 
 };
 
+// ==================================================================
+// start node.js coverage
 const startV8Coverage = async () => {
-    // start node.js coverage
     const session = new inspector.Session();
     session.connect();
-
     await session.post('Profiler.enable');
     await session.post('Profiler.startPreciseCoverage', {
         callCount: true,
         detailed: true
     });
-
     return session;
 };
 
-const collectV8Coverage = (session) => {
+const takeV8Coverage = (session) => {
     return new Promise((resolve) => {
-        session.post('Profiler.takePreciseCoverage', async (error, coverage) => {
+        session.post('Profiler.takePreciseCoverage', (error, coverage) => {
             if (error) {
                 console.log(error);
                 resolve();
                 return;
             }
-
-            let coverageList = coverage.result;
-
-            // filter node internal files
-            coverageList = coverageList.filter((entry) => entry.url && !entry.url.startsWith('node:'));
-
-            // filter node modules
-            coverageList = coverageList.filter((entry) => entry.url.indexOf('node_modules') === -1);
-
-            // node.js coverage haven't source attached
-            coverageList.forEach((item) => {
-                const filePath = fileURLToPath(item.url);
-                if (fs.existsSync(filePath)) {
-                    item.source = fs.readFileSync(filePath).toString('utf8');
-                } else {
-                    console.log('not found file', filePath);
-                }
-            });
-
-            // console.log(coverageList);
-
-            console.log('add node.js coverage');
-            await new CoverageReport(coverageOptions).add(coverageList);
-
-            resolve();
+            resolve(coverage.result);
         });
     });
+};
+// ==================================================================
+
+const collectV8Coverage = async (session) => {
+
+    let coverageList = await takeV8Coverage(session);
+    if (!coverageList) {
+        return;
+    }
+
+    // filter node internal files
+    coverageList = coverageList.filter((entry) => entry.url && !entry.url.startsWith('node:'));
+
+    // filter node modules
+    coverageList = coverageList.filter((entry) => entry.url.indexOf('node_modules') === -1);
+
+    // attached source content
+    coverageList.forEach((item) => {
+        const filePath = fileURLToPath(item.url);
+        if (fs.existsSync(filePath)) {
+            item.source = fs.readFileSync(filePath).toString('utf8');
+        } else {
+            EC.logRed('not found file', filePath);
+        }
+    });
+
+    // console.log(coverageList);
+
+    console.log('add node.js coverage ...');
+    await new CoverageReport(coverageOptions).add(coverageList);
 
 
 };
