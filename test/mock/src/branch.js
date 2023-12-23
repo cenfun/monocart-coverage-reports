9025570cc4b21710ddc87da5250c2d62dbc6bce6
cenfun <cenfun@gmail.com>
2023-12-23 14:59:30 +0800

    add ConditionalExpression

--- test/mock/src/branch.js
+++ test/mock/src/branch.js
@@ -1,31 +1,28 @@
-const LogicalExpression_11 = (tf1, tf2) => {
-    const a = tf1 || 0;
-    const b = tf2 || a || 1;
-    console.log(b);
-};
-const LogicalExpression_10 = (tf1, tf2) => {
-    const a = tf1 || 0;
-    const b = tf2 || a || 1;
-    console.log(b);
-};
-const LogicalExpression_01 = (tf1, tf2) => {
-    const a = tf1 || 0;
-    const b = tf2 || a || 1;
-    console.log(b);
-};
-const LogicalExpression_00 = (tf1, tf2) => {
-    const a = tf1 || 0;
-    const b = tf2 || a || 1;
-    console.log(b);
-};
 
-// ConditionalExpression
-// const ConditionalExpression = (tf1, tf2) => {
-//     const a = tf1 ? 'true' : 'false';
-//     console.log(a);
-//     const b = tf2 ? 'true' : 'false';
-//     console.log(b);
-// };
+const ConditionalExpression_11 = (tf1, tf2) => {
+    const a = tf1 ? 'true' : 'false';
+    console.log(a);
+    const b = tf2 ? 'true' : 'false';
+    console.log(b);
+};
+const ConditionalExpression_10 = (tf1, tf2) => {
+    const a = tf1 ? 'true' : 'false';
+    console.log(a);
+    const b = tf2 ? 'true' : 'false';
+    console.log(b);
+};
+const ConditionalExpression_01 = (tf1, tf2) => {
+    const a = tf1 ? 'true' : 'false';
+    console.log(a);
+    const b = tf2 ? 'true' : 'false';
+    console.log(b);
+};
+const ConditionalExpression_00 = (tf1, tf2) => {
+    const a = tf1 ? 'true' : 'false';
+    console.log(a);
+    const b = tf2 ? 'true' : 'false';
+    console.log(b);
+};
 
 
 // IfStatement
@@ -69,6 +66,27 @@ const LogicalExpression_00 = (tf1, tf2) => {
 
 // })();
 
+const LogicalExpression_11 = (tf1, tf2) => {
+    const a = tf1 || 0;
+    const b = tf2 || a || 1;
+    console.log(b);
+};
+const LogicalExpression_10 = (tf1, tf2) => {
+    const a = tf1 || 0;
+    const b = tf2 || a || 1;
+    console.log(b);
+};
+const LogicalExpression_01 = (tf1, tf2) => {
+    const a = tf1 || 0;
+    const b = tf2 || a || 1;
+    console.log(b);
+};
+const LogicalExpression_00 = (tf1, tf2) => {
+    const a = tf1 || 0;
+    const b = tf2 || a || 1;
+    console.log(b);
+};
+
 
 // AssignmentPattern
 // ((a = 0) => {
@@ -81,6 +99,11 @@ const branch = () => {
     LogicalExpression_10(true, false);
     LogicalExpression_01(false, true);
     LogicalExpression_00(false, false);
+
+    ConditionalExpression_11(true, true);
+    ConditionalExpression_10(true, false);
+    ConditionalExpression_01(false, true);
+    ConditionalExpression_00(false, false);
 
     // ConditionalExpression(tf1, tf2);
     // IfStatement(tf1, tf2);
