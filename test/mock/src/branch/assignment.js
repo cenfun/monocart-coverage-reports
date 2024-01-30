3aa6f14560f4e1c218a0427fe92d5f57d24673dc
cenfun <cenfun@gmail.com>
2024-01-30 21:45:45 +0800

    fixed branches for AssignmentPattern

--- test/mock/src/branch/assignment.js
+++ test/mock/src/branch/assignment.js
@@ -2,6 +2,15 @@
 // AssignmentPattern
 // no v8 coverage data for default arguments, count as covered always
 
+function param(name, url = 'url', port = 8090) {
+    if (name) {
+        return null;
+    }
+    if (url) {
+        return `${url}:${port}`;
+    }
+}
+
 const AssignmentPattern_0 = (a = 1, b = 2) => {
     console.log(a, b);
 };
@@ -19,4 +28,8 @@ module.exports = () => {
 
     AssignmentPattern(0, 0);
     AssignmentPattern();
+
+
+    param('path');
+    param('p');
 };
