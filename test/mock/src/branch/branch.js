9ba8780852ec6a7567efb9f85ee9be5adabefd4a
cenfun <cenfun@gmail.com>
2024-02-01 16:17:10 +0800

    fix branch count

--- test/mock/src/branch/branch.js
+++ test/mock/src/branch/branch.js
@@ -15,17 +15,47 @@ const uncoveredFunction = () => {
 
 function coveredFunction() {
     // branches in a block statement
-    for (let i = 0; i < 3; i++) {
-        if (i > 1) {
+    for (let i = 0; i < 5; i++) {
+        if (i > 2) {
             console.log(i);
         } else if (i > 100) {
             uncoveredFunction();
         }
     }
+
+    const list = [1, 2, 3, 4, 5];
+    list.forEach((v) => {
+        if (v > 2) {
+            console.log(v);
+            return;
+        }
+        console.log(v);
+    });
+
+    for (const item of list) {
+        if (item > 3) {
+            console.log(item);
+            break;
+        }
+        console.log(item);
+    }
+
+    const l = 5;
+    let i = 0;
+    while (i < l) {
+        if (i < 3) {
+            console.log(i);
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+
 }
 
 const branch = () => {
 
+    coveredFunction();
     coveredFunction();
 
     AssignmentPattern();
