28c38986982db2142642101ff0e4ad0edcc50c9e
cenfun <cenfun@gmail.com>
2024-02-28 13:11:40 +0800

    added new option `cleanCache`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 * 2.5.9
     - fixed issue for istanbul source and source path replacement
     - fixed issue for original file when fully uncovered
+    - added new option `cleanCache`
 
 * 2.5.8
     - added new option `all` for adding empty coverage for all files
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -280,8 +280,15 @@ declare namespace MCR {
         * {object} { bytes:[50,80], statements:[50,80], branches:[50,80], functions:[50,80], lines:[50,80] } */
         watermarks?: Watermarks;
 
-        /** {boolean} clean previous files in output dir before generating report. Defaults to true. */
+        /** {boolean} Indicates whether to clean previous files in output dir before generating report. Defaults to true. */
         clean?: boolean;
+
+        /**
+         * {boolean} Indicates whether to clean previous cache in output dir before generating report. Defaults to false. 
+         * 
+         * If true, the API `cleanCache()` will execute automatically.
+        */
+        cleanCache?: boolean;
 
         /** {function} onEnd hook */
         onEnd?: (reportData: CoverageResults) => Promise<void>;
