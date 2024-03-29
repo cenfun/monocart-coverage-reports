e41ebeac0fbbdcec6363855bb57c97de465ea071
cenfun <cenfun@gmail.com>
2024-03-29 08:00:38 +0800

    added logging for debug

--- test/test-node-env.js
+++ test/test-node-env.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 // remove previous coverage files
 const dir = process.env.NODE_V8_COVERAGE;
 if (fs.existsSync(dir)) {
-    fs.rmSync(dir, {
-        recursive: true,
-        force: true
-    });
+    try {
+        fs.rmSync(dir, {
+            recursive: true,
+            force: true,
+            maxRetries: 10
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
 // =====================================================
