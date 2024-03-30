f326c24ef5055c97fea20f2d370fce84df811647
cenfun <cenfun@gmail.com>
2024-03-30 11:28:21 +0800

    fixed unit test for linux

--- test/unit/source-path.test.js
+++ test/unit/source-path.test.js
@@ -1,11 +1,11 @@
-const os = require('os');
+const platform = process.platform;
 const assert = require('assert');
 const { resolveSourceUrl, getSourcePath } = require('../../lib/utils/source-path.js');
 
 it('resolveSourceUrl', () => {
 
     // windows absolute path
-    if (os.platform() === 'win32') {
+    if (platform === 'win32') {
         console.log('test win32 path');
         assert.equal(getSourcePath(resolveSourceUrl('C://a.js')), 'C/a.js');
         assert.equal(getSourcePath(resolveSourceUrl('C:\\\\a.js')), 'C/a.js');
@@ -22,7 +22,18 @@ it('resolveSourceUrl', () => {
     assert.equal(getSourcePath(resolveSourceUrl('webpack://coverage-v8/./src/branch.js')), 'coverage-v8/src/branch.js');
 
     // file:// protocol
-    assert.equal(getSourcePath(resolveSourceUrl('file://src/a.js')), 'src/a.js');
+    if (platform === 'win32') {
+        // absolute
+        assert.equal(getSourcePath(resolveSourceUrl('file:///C:/path/src/a.js')), 'C/path/src/a.js');
+        // relative
+        assert.equal(getSourcePath(resolveSourceUrl('file://src/a.js')), 'src/a.js');
+    } else {
+        // absolute ///
+        assert.equal(getSourcePath(resolveSourceUrl('file:///src/a.js')), 'src/a.js');
+        // relative localhost or empty
+        assert.equal(getSourcePath(resolveSourceUrl('file://localhost/src/a.js')), 'src/a.js');
+        assert.equal(getSourcePath(resolveSourceUrl('file:/src/a.js')), 'src/a.js');
+    }
 
     // relative
     assert.equal(getSourcePath(resolveSourceUrl('../../src/a.js')), 'src/a.js');
@@ -34,8 +45,18 @@ it('resolveSourceUrl', () => {
     assert.equal(getSourcePath(resolveSourceUrl('a.js', 'dist')), 'dist/a.js');
     assert.equal(getSourcePath(resolveSourceUrl('../a.js', 'dist')), 'a.js');
 
-    assert.equal(getSourcePath(resolveSourceUrl('./a.js', 'file://dist')), 'dist/a.js');
-    assert.equal(getSourcePath(resolveSourceUrl('../a.js', 'file://dist/')), 'dist/a.js');
+    if (platform === 'win32') {
+        assert.equal(getSourcePath(resolveSourceUrl('./a.js', 'file:///C:/path/src')), 'C/path/src/a.js');
+        assert.equal(getSourcePath(resolveSourceUrl('../a.js', 'file:///C:/path/src')), 'C/path/a.js');
+
+        assert.equal(getSourcePath(resolveSourceUrl('./a.js', 'file://path/src')), 'path/src/a.js');
+        assert.equal(getSourcePath(resolveSourceUrl('../a.js', 'file://path/src')), 'path/a.js');
+
+    } else {
+        // absolute ///
+        assert.equal(getSourcePath(resolveSourceUrl('./a.js', 'file:///path/src')), 'path/src/a.js');
+        assert.equal(getSourcePath(resolveSourceUrl('../a.js', 'file:///path/src')), 'path/a.js');
+    }
 
     assert.equal(getSourcePath(resolveSourceUrl('a.js', '/root')), 'root/a.js');
 
