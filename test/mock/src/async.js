6a4360272460b8b5011b0c51a03360cce893fcaa
cenfun <cenfun@gmail.com>
2024-06-16 00:25:14 +0800

    added test case for `try catch await`
    fixed snapshot order
    fixed `}` coverage for bytes

--- test/mock/src/async.js
+++ test/mock/src/async.js
@@ -11,10 +11,12 @@ function apple(awesome) {
     }
 }
 
+// eslint-disable-next-line no-unused-vars
 function missed() {
 
 }
 
+// eslint-disable-next-line no-unused-vars
 function missed2() {
 
 }
@@ -22,3 +24,31 @@ function missed2() {
 apple();
 apple();
 apple();
+
+
+const try_catch_await = async () => {
+    const list = [new Promise((resolve, reject) => {
+        setTimeout(resolve, 100);
+    }), new Promise((resolve, reject) => {
+        setTimeout(reject, 100);
+    })];
+
+    try {
+        for (const p of list) {
+            console.log(await p);
+        }
+        console.log('uncovered');
+    } catch (err) {
+        console.log(err);
+    }
+
+    try {
+        for await (const p of list) {
+            console.log(p);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+try_catch_await();
