af7c2e0a4608f6f0ec791b7421a997db2672f652
CenFun <cenfun@qq.com>
2026-05-29 08:42:51 +0800

    fix performance

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,13 @@ const vue = require('eslint-plugin-vue');
 // https://eslint.org/docs/latest/use/configure/configuration-files
 module.exports = [
     {
+        ignores: [
+            'lib/packages/',
+            '**/dist/',
+            '**/node_modules/'
+        ]
+    },
+    {
         languageOptions: {
             globals: {
                 Bun: 'readonly'
